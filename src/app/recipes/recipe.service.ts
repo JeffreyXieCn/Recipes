11e5b8fc245cc1f2f0ef665fba7e3742ca779b83
Jeffrey XIE <jeffrey.xie@ericsson.com>
2019-08-31 16:23:14 -0400

    Finish recipe edit reactive form

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,10 +2,12 @@ import { Recipe } from './recipe.model';
 // import { EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 // import { Subject } from 'rxjs';
 
 export class RecipeService {
   // recipeSelected = new Subject<Recipe>();
+  recipesChanged = new Subject<Recipe[]>();
   private recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
@@ -13,18 +15,14 @@ export class RecipeService {
       'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg',
       [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
-    new Recipe(
-      'Second Recipe',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg',
-      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
-    ),
-    new Recipe(
-      'Third Recipe',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg',
-      [new Ingredient('Tomatoes', 3), new Ingredient('Eggs', 4)]
-    )
+    new Recipe('Second Recipe', 'A tasty shrimp', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg', [
+      new Ingredient('Buns', 2),
+      new Ingredient('Shrimps', 10)
+    ]),
+    new Recipe('Third Recipe', 'This is a meat stick', 'https://www.nps.gov/subjects/camping/images/recipe_1.jpg', [
+      new Ingredient('Tomatoes', 3),
+      new Ingredient('Eggs', 4)
+    ])
   ];
 
   constructor(private slService: ShoppingListService) {}
@@ -39,5 +37,15 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
